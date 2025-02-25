d935a8ee9c1c456b841cd888eef786b27df824dc
Renan Toniolo Rocha <renantoniolo@gmail.com>
2025-02-25 20:51:58 -0300

    Initial Commit

---
+++ fruits/fruitsApp.swift
@@ -0,0 +1,17 @@
+//
+//  fruitsApp.swift
+//  fruits
+//
+//  Created by Renan Toniolo Rocha on 25/02/25.
+//
+
+import SwiftUI
+
+@main
+struct fruitsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
